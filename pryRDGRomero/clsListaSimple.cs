4240d3e4f657a16fab99c6c603c5cf515614614b
Gimena <rgclaudia073@gmail.com>
2026-05-10 00:11:43 -0300

    listasimple

---
+++ pryRDGRomero/clsListaSimple.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryRDGRomero
+{
+    internal class clsListaSimple
+    {
+        //Campos de clase
+        private clsNodo pri;
+
+        //Propiedades de clase
+        public clsNodo Pri
+        {
+            get { return pri; }
+            set { pri = value; }
+        }
+
+
+        public void Agregar(clsNodo Nuevo)
+        {
+            if (Pri == null)
+            {
+                Pri = Nuevo;
+            }
+            else
+            {
+                if (Nuevo.Código < Pri.Código)
+                {
+                    Nuevo.Sig = Pri;
+                    Pri = Nuevo;
+                }
+                else
+                {
+                    clsNodo aux = pri;
+                    clsNodo ant = Pri;
+                    while (Nuevo.Código > aux.Código)
+                    {
+                        ant = aux;
+                        aux = aux.Sig;
+                    }
+                    Nuevo.Sig = aux;
+                    ant.Sig = Nuevo;
+                }
+            }
+
+        }
+    }
+}
